302a83f1da8235b160e7fdde9b8ad4ea7bd3a790
Denis Kyashif <dkyashif@worldbankgroup.org>
2023-03-29 17:46:18 +0300

    update listener

--- Listener/ListenerWorker.cs
+++ Listener/ListenerWorker.cs
@@ -20,13 +20,18 @@ public class ListenerWorker : BackgroundService
         
         var receiver = client.CreateReceiver(_options.QueueName, new ServiceBusReceiverOptions
         {
-            ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete
+			// The receiver has a lock on the message for a particular duration.
+            // If the message is not settled by this time, it lands back on Service Bus
+            // to be fetched by the next receive operation
+			ReceiveMode = ServiceBusReceiveMode.PeekLock
 		});
         
         while (!stoppingToken.IsCancellationRequested)
         {
             var receivedMessage = await receiver.ReceiveMessageAsync();
             _logger.LogInformation("Received message: {0}", receivedMessage.Body);
+            
+            await receiver.CompleteMessageAsync(receivedMessage); // Deletes the message from the queue
         }
     }
 }
